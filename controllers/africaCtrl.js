6a96dd9cb99d6072d4108b2aab60c9adacf1fc3f
Erin Meyers <erinmeyers88@gmail.com>
2015-11-10 10:18:51 -0700

    Added basic collapsible list.

--- controllers/africaCtrl.js
+++ controllers/africaCtrl.js
@@ -1,25 +1,33 @@
 angular.module("travelTracker")
 	.controller("africaCtrl", function ($scope, countriesService) {
-		
+
 		$scope.getCountries = function () {
-			
+
 			countriesService.getAfrica().then(function (result) {
 				$scope.region = result;
-			});	
-		}();
-				
+			});
+		} ();
+
 		$scope.updateVisitedOrNot = function (country, visitedOrNot) {
-			countriesService.updateVisitedOrNot(country, visitedOrNot);	
+			countriesService.updateVisitedOrNot(country, visitedOrNot);
 		};
-	
+
 		$scope.visitedCountries = countriesService.visitedCountries;
-		
+
 		$scope.checkCountry = function (country) {
 			if ($scope.visitedCountries.indexOf(country.name) !== -1) {
 				return true;
 			}
 		};
-		
-		
-		
+
+		$scope.accordion = {
+			current: null
+		};
+
+		$scope.collapsed = true;
+
+		$scope.collapse = function () {
+			$scope.collapsed = !$scope.collapsed;
+		};
+
 	});
